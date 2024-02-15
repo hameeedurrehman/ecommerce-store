fd95fd0386e5b0fd348cb84f2e710cdd31315555
Hameed Ur Rehman <hameedurrehman35@gmail.com>
2024-02-15 23:18:44 +0500

    simplified header

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,17 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        tertiary: "var(--color-tertiary)",
+        success: "var(--color-success)",
+        error: "var(--color-error)",
+        warning: "var(--color-warning)",
+        info: "var(--color-info)",
+        dark: "var(--color-dark)",
+        light: "var(--color-light)",
+      },
     },
   },
   plugins: [],
