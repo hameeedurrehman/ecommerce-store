922f54f5fce2c4769a759b4d74b41adfd92b59b4
Hameed Ur Rehman <hameedurrehman35@gmail.com>
2024-02-25 12:47:09 +0500

    added logo

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,19 +1,32 @@
 "use client";
 import Checkout from "@components/checkout/Checkout";
+import Image from "next/image";
 import Link from "next/link";
 
-const pages = ["Products"];
+const pages = ["Categories","Products", "About", "Contact", "Register"];
 const settings = ["Profile", "Dashboard", "Logout"];
 
 function Header() {
   return (
-    <div className="bg-primary py-5">
-      <div className="flex justify-between container mx-auto">
+    <div className="bg-primary py-5 text-white">
+      <div className="flex justify-center space-x-10 items-center container mx-auto">
+        <div>
+          <Link href={"/"}>
+            <Image
+              src="/assets/images/logo.jpg"
+              width={80}
+              height={80}
+              alt="logo"
+            />
+          </Link>
+        </div>
+        <div className="flex">
         {pages.map((page) => (
           <Link href={`/${page.toLowerCase()}`} key={page} className="mr-4">
             {page}
           </Link>
         ))}
+        </div>
         <Checkout />
       </div>
     </div>
